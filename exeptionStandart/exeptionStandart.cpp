203109cec2a66ab36bac313ec34fb45546d66c5e
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-19 15:38:31 +0700

    penanda 1:

--- exeptionStandart/exeptionStandart.cpp
+++ exeptionStandart/exeptionStandart.cpp
@@ -3,3 +3,8 @@
 //untuk exception yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main() {
+	cout << "Awal program" << endl; //penanda 1:
+}
