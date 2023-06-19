1851d8acc80f648bf7f7879ba808df12e10e0fe5
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-06-19 15:34:16 +0700

    jika selain int maka blok ini akan dieksekusi

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main() {
 	try {
 		cout << " Selamat belajar di prodi ti UMY" << endl;
-		throw 1;
+		throw 1; //melemparkan sebuah intereger maka
+		cout << "Pertanyaan tidak akan di eksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*jika selain int maka blok ini akan dieksekusi*/
 	}
 }
